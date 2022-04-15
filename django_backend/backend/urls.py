e4d9f6228e06bc90ea7d90f0c69d92ca4735d1e3
Aleks Kudryashov <aleks59niki30@gmail.com>
2022-04-15 18:17:18 +0300

    add urls for backend

--- django_backend/backend/urls.py
+++ django_backend/backend/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 
+from backend.views import RegisterAccount, ConfirmAccount
+
 app_name = 'backend'
 
-urlpatterns = []
+urlpatterns = [
+    # Регистрация пользователя и подтверждение электронной почты
+    path('user/register', RegisterAccount.as_view(), name='user-register'),
+    path('user/register/confirm', ConfirmAccount.as_view(), name='user-register-confirm'),
+]
