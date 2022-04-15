319d32730d2b669a95aec2d99fe1f0ebb710b2e2
Aleks Kudryashov <aleks59niki30@gmail.com>
2022-04-15 18:14:55 +0300

    add views for user reg

--- django_backend/backend/views.py
+++ django_backend/backend/views.py
@@ -1,3 +1,69 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from rest_framework.views import APIView
+
+from django.contrib.auth.password_validation import validate_password
+
+from backend.serializers import UserSerializer
+
+from backend.signals import new_user_registered
+
+from backend.models import ConfirmEmailToken
+
+
+class RegisterAccount(APIView):
+    """Регистрация пользователя"""
+
+    def post(self, request, *args, **kwargs):
+        if {'first_name', 'last_name', 'email', 'password'}.issubset(request.data):
+            try:
+                # Проверка пароля на сложность
+                validate_password(request.data['password'])
+            except Exception as password_error:
+                error_array = []
+                # noinspection PyTypeChecker
+                for item in password_error:
+                    error_array.append(item)
+                return JsonResponse({'Status': False, 'Errors': {'password': error_array}})
+            else:
+                # Проверка на уникальность имени пользователя
+                request.data._mutable = True
+                request.data.update({})
+
+                user_serializer = UserSerializer(data=request.data)
+
+                if user_serializer.is_valid():
+                    user = user_serializer.save()
+                    user.set_password(request.data['password'])
+                    user.save()
+
+                    new_user_registered.send(sender=self.__class__, user_id=user.id)
+
+                    return JsonResponse({'Status': True})
+                else:
+                    return JsonResponse({'Status': False, 'Errors': user_serializer.errors})
+
+        return JsonResponse({'Status': False, 'Errors': 'Не указаны все необходимые аргументы'})
+
+
+class ConfirmAccount(APIView):
+    """Класс для подтверждения почтового адреса"""
+
+    def post(self, request, *args, **kwargs):
+        if {'email', 'token'}.issubset(request.data):
+
+            token = ConfirmEmailToken.objects.filter(
+                user__email=request.data['email'],
+                key=request.data['token']
+            ).first()
+
+            if token:
+                token.user.is_active = True
+                token.user.save()
+                token.delete()
+
+                return JsonResponse({'Status': True})
+            else:
+                return JsonResponse({'Status': False, 'Errors': 'Неправильно указан токен или email'})
+
+        return JsonResponse({'Status': False, 'Errors': 'Не указаны все необходимые аргументы'})
