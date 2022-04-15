aafa231932072386d7dc4c80f4528e6f14b56148
Aleks Kudryashov <aleks59niki30@gmail.com>
2022-04-15 18:11:18 +0300

    create user serializer

---
+++ django_backend/backend/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from backend.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'first_name', 'last_name', 'email')
+        read_only_fields = ('id',)
