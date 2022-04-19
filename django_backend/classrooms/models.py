ffbe5391b98e23de2f2f721d9f042050ef6b6078
Alexandr Masnoy <icecubeloc@gmail.com>
2022-04-19 22:34:38 +0300

    Schedule, Homework models created

--- django_backend/classrooms/models.py
+++ django_backend/classrooms/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from accounts.models import User
 from courses.models import Course
+from modules.models import Lesson
 
 
 class Classroom(models.Model):
@@ -34,9 +35,6 @@ class StudentClassroom(models.Model):
 
     is_completed = models.BooleanField(default=False, verbose_name='Закончил обучение')
 
-    def __str__(self):
-        return f'{self.classroom.title} - {self.student.email}'
-
     class Meta:
         verbose_name = 'Учебная группа ученика'
         verbose_name_plural = 'Учебные группы учеников'
@@ -46,3 +44,41 @@ class StudentClassroom(models.Model):
         constraints = [
             models.UniqueConstraint(fields=['classroom', 'student'], name='unique_student_classroom'),
         ]
+
+
+class Schedule(models.Model):
+    """Расписание"""
+
+    teacher = models.ForeignKey(User, verbose_name='Преподователь', on_delete=models.SET_NULL,
+                                blank=True, null=True)
+    classroom = models.ForeignKey(Classroom, verbose_name='Группа', on_delete=models.SET_NULL,
+                                  blank=True, null=True)
+    subject = models.ForeignKey(Lesson, verbose_name='Предмет', on_delete=models.PROTECT,
+                                blank=True, null=True)
+    date_of_lesson = models.DateField(blank=True, null=True, verbose_name='Дата проведения занятия')
+
+    def __str__(self):
+        return f'{self.subject.title} - {self.classroom.title}'
+
+    class Meta:
+        verbose_name = 'Расписание'
+        verbose_name_plural = 'Расписания'
+
+
+class Homework(models.Model):
+    """Домашнее задание ученика"""
+    homework = models.URLField(verbose_name='Ссылка на домашнее задание', max_length=200,
+                               blank=True, null=True)
+    student = models.ForeignKey(User, verbose_name='Ученик', on_delete=models.CASCADE,
+                                blank=True, null=True)
+    date_of_publication = models.DateField(auto_now_add=True)
+    is_accepted = models.BooleanField(default=False, verbose_name='Домашняя работа выполнена верно')
+    schedule = models.ForeignKey(Schedule, verbose_name='Урок', on_delete=models.CASCADE,
+                                 blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.schedule.title} - {self.classroom.title}'
+
+    class Meta:
+        verbose_name = 'Домашнее задание'
+        verbose_name_plural = 'Домашние задания'
