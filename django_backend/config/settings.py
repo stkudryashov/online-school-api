233e9d2bd05f4e71be693b42d57a97c6060f64bd
Aleks Kudryashov <aleks59niki30@gmail.com>
2022-04-30 01:09:16 +0300

    env CSRF_TRUSTED_ORIGINS

--- django_backend/config/settings.py
+++ django_backend/config/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:1337']
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS').split(' ')
 
 
 # Application definition
