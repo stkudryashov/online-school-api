88022dfd00c9f40bb3db80f40850ec16fa7d035c
Aleks Kudryashov <aleks59niki30@gmail.com>
2022-04-30 03:18:35 +0300

    add simple html page

--- django_backend/config/settings.py
+++ django_backend/config/settings.py
@@ -73,10 +73,13 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'config.urls'
 
+
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- django_backend/config/urls.py
+++ django_backend/config/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
 urlpatterns = [
+    path('', index),
     path('admin/', admin.site.urls),
 
     path('api/v1/', include('accounts.urls', namespace='accounts')),
